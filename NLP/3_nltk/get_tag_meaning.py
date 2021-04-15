f58b36f7348a260606840bff0e133e758b596d64
ybdesire <ybdesire@gmail.com>
2021-04-15 16:51:00 +0800

    get nltk tag meaning

---
+++ NLP/3_nltk/get_tag_meaning.py
@@ -0,0 +1,10 @@
+import nltk
+
+nltk.help.upenn_tagset('CC')
+
+```
+CC: conjunction, coordinating
+    & 'n and both but either et for less minus neither nor or plus so
+    therefore times v. versus vs. whether yet
+```
+
