e0dbb948c6670945710d281a152b968c362099bf
ybdesire <ybdesire@gmail.com>
2021-04-15 16:51:17 +0800

    Update get_tag_meaning.py

--- NLP/3_nltk/get_tag_meaning.py
+++ NLP/3_nltk/get_tag_meaning.py
@@ -2,9 +2,12 @@ import nltk
 
 nltk.help.upenn_tagset('CC')
 
-```
+'''
 CC: conjunction, coordinating
     & 'n and both but either et for less minus neither nor or plus so
     therefore times v. versus vs. whether yet
-```
+'''
+
+
+
 
